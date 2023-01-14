d89f45c9210e47a5a98d1d5756e41139f4f2109a
Abdalla Moha <58549282+shaalle@users.noreply.github.com>
2023-01-14 14:25:13 +0000

    update

--- src/App.js
+++ src/App.js
@@ -6,21 +6,33 @@ function App() {
   return (
     <div className="App">
       <div>
-        <User />
-        <User />
-        <User />
+        <User name="Sucdi" age={28} email="drssucdi@gmail.com"/>
+        <User name="Jamaal" age={39} email="jamal@gmail.com"/>
+        <User name="Ridwaan" age={16} email="Rid@gmail.com"/>
+        <Jobs salary={90000} position="Senior SDE" company="Amazon"/>
       </div>
     </div>
   );
 
 }
-const User = () => {
+// This is a Component 
+const User = (props) => {
   return (
     <div>
-      <h1>Sucdi</h1>
-      <h2>28</h2>
-      <h2>drssucdi@gmail.com</h2>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h2>{props.email}</h2>
     </div>
   );
+}
+
+const Jobs = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  )
 }
 export default App;
