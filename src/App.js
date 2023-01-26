1f9ac8df702f276c1f6a76596643bf3a5f3d7496
shaalle <abdallamoha64@gmail.com>
2023-01-26 21:54:14 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import "./App.css";
-import Axios from "axios";
-import { useState, createContext } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Menu } from "./pages/Menu";
-import { NavBar } from "./Navbar";
-import { Profile } from "./pages/Profile";
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-export const AppContext = createContext();
+import { Form } from "./components/Form";
+
 function App() {
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: true
-      }
-    }
-  });
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
-          </Routes>
-        </Router>
-        </QueryClientProvider>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,30 @@
+import {useForm} from 'react-hook-form';
+import * as yup from 'yup';
+export const Form = () => {
+    const schema = yup.object().shape({
+        fullName: yup.string().required(),
+        email: yup.string().email().required(),
+        age: yup.number().positive().integer().min(18).required(),
+        password: yup.string().min(4).max(20).required(),
+        confirmPassword: yup.string().oneOf([yup.ref("pasword"), null]).required()
+    });
+
+    const {register, handleSubmit} = useForm({
+        resolver: 
+    });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+    return (
+        <form className='form' onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Full Name..." {...register("fullName")}/>
+            <input type="text" placeholder="Email..." {...register("email")}/>
+            <input type="number" placeholder="Age..." {...register("age")}/>
+            <input type="password" placeholder="Password..." {...register("password")}/>
+            <input type="password" placeholder="Confirm Password..." {...register("confirmPassword")}/>
+            <input type="submit" />
+        </form>
+    );
+}
